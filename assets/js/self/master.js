ddbbbca1f1db8be24335e0007a605ce2b415da64
viethdd <viethdd@yahoo.com>
2014-09-09 12:53:35 -0700

    css3-addcolor2balls

--- assets/js/self/master.js
+++ assets/js/self/master.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 		$('.top-bar-btn2').addClass('top-btn-active');
 	});
 
-//toggle top-bar and activate top-bar btn on each btn
+	//toggle top-bar and activate top-bar btn on each btn
 	$('.top-bar-btn').each(function(){
 		$(this).on('click',function(){
 			var i = $(this).index();
@@ -26,49 +26,85 @@ $(document).ready(function(){
 		});
 	});
 
-//add up & down keyboard events
-$('body,html').keydown(function(event){
-	var index = $('.top-btn-active').data('index');
+	//add up & down keyboard events
+	$('body,html').keydown(function(event){
+		var index = $('.top-btn-active').data('index');
 
-	//pressing down event
-	if(event.which == 40){
-		console.log('ok dude');
-		scrollStep(index);
-		console.log(index);
-	}
-
-	if(event.which == 38){
-		console.log('ok dude');
-		// scrollStep((index-1));
-		console.log(index);
-		// scrollStep(index-1);
-		// $('.top-bar-btn').removeClass('top-btn-active');
-		// $('.top-bar-btn'+(index+1)).addClass('top-btn-active');
-	}
-
-
-	// else return true;
-});
-
-
-
-//do something else
-$(window).scroll(function(){
-    var offsetTop = $(window).scrollTop();
-		offsetIndex();
-    function offsetIndex(){
-			var offIndex = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
-			var offset = 0;
-			for (i in offIndex){
-				if (offsetTop >= offset){
-					$('.top-bar-btn').removeClass('top-btn-active');
-					$('.top-bar-btn'+offIndex[i]).addClass('top-btn-active');
-
-				}
-				offset+=750;
-			}
+		//pressing down event
+		if(event.which == 40){
+			console.log('ok dude');
+			scrollStep(index);
+			console.log(index);
 		}
-  });
 
+		if(event.which == 38){
+			console.log('ok dude');
+			// scrollStep((index-1));
+			console.log(index);
+			// scrollStep(index-1);
+			// $('.top-bar-btn').removeClass('top-btn-active');
+			// $('.top-bar-btn'+(index+1)).addClass('top-btn-active');
+		}
+		// else return true;
+	});
+
+
+	//do something else
+	$(window).scroll(function(){
+	    var offsetTop = $(window).scrollTop();
+			
+			offsetIndex();
+	    function offsetIndex(){
+				var offIndex = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+				var offset = 0;
+				for (i in offIndex){
+					if (offsetTop >= offset){
+						$('.top-bar-btn').removeClass('top-btn-active');
+						$('.top-bar-btn'+offIndex[i]).addClass('top-btn-active');
+					}
+					offset+=750;
+				}
+				
+			}
+	  });
+
+	$('.ease-keyword').each(function(){
+		$(this).bind('click',function(){
+			var eIndex = $(this).data('ease');
+			scrollStep(eIndex);
+		});
+	});
+
+	$('.info-img').each(function(){
+		$(this).bind('click',function(){
+			var eiIndex = $(this).data('easeinfo');
+			scrollStep(eiIndex);
+		});
+	});
+
+	$('.arrow--down').bind('click',function(){
+		var offsetTop = $(window).scrollTop();
+		var step = $('.nav-arrows-wrapper').data('step')+1;
+		if (offsetTop >= 0){
+		  scrollStep(step);
+		  step++;
+		  $('.nav-arrows-wrapper').data('step',step);	
+		  $('.arrow--up').removeClass('arrow--init');
+		}
+		
+	});
+
+	$('.arrow--up').bind('click',function(){
+		var offsetTop = $(window).scrollTop();
+		var step = $('.nav-arrows-wrapper').data('step')-1;
+		if (offsetTop >= 0){
+		  scrollStep(step-1);
+		  step--;
+		  $('.nav-arrows-wrapper').data('step',step);	
+		  
+		}
+		
+	});
+	
 
 });
